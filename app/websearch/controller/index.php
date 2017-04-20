3c1e59c3cb5985b830fbafdf5c2b4437fb4f9a5b
stormhu <stormhu@foxmail.com>
2017-04-20 17:31:09 +0800

    office

---
+++ app/websearch/controller/index.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | 
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: stormhu <stormgu@foxmail.com>
+// +----------------------------------------------------------------------
+namespace app\websearch\controller;
+use think\Controller;
+
+// 
+// 
+// 
+class index extends Controller
+{
+	public function index(){
+		
+	}
+}
